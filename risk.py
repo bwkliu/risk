65689f01433c771563d1e41963713561de685f52
bwkliu <bwkliu@qq.com>
2017-04-08 02:30:07 +0800

    xgb_train

--- risk.py
+++ risk.py
@@ -1,7 +1,13 @@
 import  pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+import xgboost as xgb
+from sklearn import cross_validation, metrics
+from sklearn.grid_search import GridSearchCV
 
+import matplotlib.pylab as plt
+
+from xgboost.core import DMatrix
 
 class Risk:
     def __init__(self):
@@ -78,17 +84,51 @@ class Risk:
         except Exception,e:
             print e
             print x
- 
-    def train(self,master_train):
-        X_train = master_train[ [x for x in master_train.columns if x!='target' ]   ] 
-        Y_train = master_train[ ['target' ]   ]
-           
-
+        return master_train
+    
+    def xgb_train(self,master_train):
+        #X_train = master_train[ [x for x in master_train.columns if x!='target' ]   ] 
+        #Y_train = master_train[ ['target' ]   ]
+        
+        xgb1 = xgb.XGBClassifier( learning_rate =0.1, n_estimators=1000, max_depth=5, min_child_weight=1, gamma=0, subsample=0.8, 
+                                  colsample_bytree=0.8, objective= 'binary:logistic', nthread=32, scale_pos_weight=1, seed=27)
+            
+        predictors = [x for x in master_train.columns if x!='target' ]
+        target = 'target'
+        self.modelfit(xgb1,master_train,target,predictors)
+        
+    
+    def modelfit(self,alg, dtrain, target ,predictors,useTrainCV=True, cv_folds=5, early_stopping_rounds=50):
+        try:
+            if useTrainCV:
+                xgb_param = alg.get_xgb_params()
+                xgtrain = xgb.DMatrix(dtrain[predictors].values, label=dtrain[target].values)
+                cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=alg.get_params()['n_estimators'], nfold=cv_folds,
+                    metrics='auc', early_stopping_rounds=early_stopping_rounds)
+                alg.set_params(n_estimators=cvresult.shape[0])
+            
+            #Fit the algorithm on the data
+            alg.fit(dtrain[predictors], dtrain[target],eval_metric='auc')
+            
+            #Predict training set:
+            dtrain_predictions = alg.predict(dtrain[predictors])
+            dtrain_predprob = alg.predict_proba(dtrain[predictors])[:,1]
+        
+            #Print model report:
+            print "\nModel Report"
+            print "Accuracy : %.4g" % metrics.accuracy_score(dtrain[target].values, dtrain_predictions)
+            print "AUC Score (Train): %f" % metrics.roc_auc_score(dtrain[target], dtrain_predprob)
+            
+            feat_imp = pd.Series(alg.booster().get_fscore()).sort_values(ascending=False)
+            feat_imp.plot(kind='bar', title='Feature Importances')
+            plt.ylabel('Feature Importance Score')    
+        except Exception,e:
+            print e
 if __name__ == '__main__':
     risk = Risk()
     master_train = risk.handle_data_nan()
-    risk.handle_data_pre(master_train)
-    risk.train(master_train)
+    master_train = risk.handle_data_pre(master_train)
+    risk.xgb_train(master_train)
     
     
     
