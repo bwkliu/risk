b499b0afc68b2c1278706f40322686a10856e036
bwkliu <bwkliu@qq.com>
2017-04-08 01:23:39 +0800

    modify

--- risk.py
+++ risk.py
@@ -2,6 +2,7 @@ import  pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
+
 class Risk:
     def __init__(self):
         self.train_filePath='/data/risk/PPD-First-Round-Data-Updated/PPD-First-Round-Data-Update/Training Set/'
@@ -29,49 +30,68 @@ class Risk:
 
     def handle_data_nan(self):
         master_train=pd.read_csv(self.master_train_file,encoding='GBK',sep=',')
-        for x,y in zip(self.fieldtype.field_name,self.fieldtype.field_type):
-            nan_rate = master_train[x].count()/float(master_train[x].size)
-            if nan_rate < 0.7:
-                master_train = master_train.drop([x],axis=1)
-            elif y == 'Categorical':
-                if master_train[x].dtypes == np.int64:
-                    master_train[x][ pd.isnull(master_train[x]) ] = 99999
-            elif master_train[x].dtypes == np.float64:
-                master_train[x][ pd.isnull(master_train[x]) ] = 99999 
-            elif master_train[x].dtypes == np.object:
-                master_train[x][ pd.isnull(master_train[x]) ] = '99999'
-            elif y == 'Numerical':
-                if master_train[x].dtypes == np.int64:
-                    master_train[x][ pd.isnull(master_train[x]) ] = master_train[x].mean()
-                elif master_train[x].dtypes == np.float64:
-                    master_train[x][ pd.isnull(master_train[x]) ] = master_train[x].mean()
+        x=None
+        y=None
+        try:
+            for x,y in zip(self.fieldtype.field_name,self.fieldtype.field_type):
+                nan_rate = master_train[x].count()/float(master_train[x].size)
+                if nan_rate < 0.7:
+                    master_train = master_train.drop([x],axis=1)
+                elif y == 'Categorical':
+                    if master_train[x].dtypes == np.int64:
+                        master_train[x].loc[ pd.isnull(master_train[x]) ] = 99999
+                    elif master_train[x].dtypes == np.float64:
+                        master_train[x].loc[ pd.isnull(master_train[x]) ] = 99999 
+                    elif master_train[x].dtypes == np.object:
+                        master_train[x].loc[ pd.isnull(master_train[x]) ] = '99999'
+                elif y == 'Numerical':
+                    if master_train[x].dtypes == np.int64:
+                        master_train[x].loc[ pd.isnull(master_train[x]) ] = master_train[x].mean()
+                    elif master_train[x].dtypes == np.float64:
+                        master_train[x].loc[ pd.isnull(master_train[x]) ] = master_train[x].mean()
        
-        list_info_time = pd.to_datetime(master_train.ListingInfo)
-        master_train['year'] = list_info_time.apply(lambda x : x.year)
-        master_train['month'] = list_info_time.apply(lambda x : x.month)
-        master_train['day'] = list_info_time.apply(lambda x : x.day)
-        master_train = master_train.drop(['ListingInfo'],axis=1)
-        master_train['year'] = master_train['year'].astype('category') 
-        master_train['month'] = master_train['month'].astype('category') 
-        master_train['day'] = master_train['day'].astype('category')         
+                 
 
- 
+        except Exception,e:
+            print e
+            print x
         master_train.to_csv('/data/risk/master_nan.csv',encoding='GBK')          
         return master_train
    
     def handle_data_pre(self,master_train):
         le = LabelEncoder()
-        for x in master_train.columns:
-            if self.fieldtype[self.fieldtype.field_name == x].field_type == 'Categorical':
-                le.fit(master_train[x].values)
-                master_train[x] = le.transform(master_train[x])
-
-         
+        x=None
+        try:
+            for x in master_train.columns:
+                if self.fieldtype[self.fieldtype.field_name == x].field_type.get_values()[0] == 'Categorical':
+                    le.fit(master_train[x].values)
+                    master_train[x] = le.transform(master_train[x])
+            
+            list_info_time = pd.to_datetime(master_train.ListingInfo)
+            master_train['year'] = list_info_time.apply(lambda x : x.year)
+            master_train['month'] = list_info_time.apply(lambda x : x.month)
+            master_train['day'] = list_info_time.apply(lambda x : x.day)
+            master_train = master_train.drop(['ListingInfo'],axis=1)
+            #master_train['year'] = master_train['year'].astype('category') 
+            #master_train['month'] = master_train['month'].astype('category') 
+            #master_train['day'] = master_train['day'].astype('category')
+        except Exception,e:
+            print e
+            print x
  
     def train(self,master_train):
         X_train = master_train[ [x for x in master_train.columns if x!='target' ]   ] 
-        Y_train = master_train[ ['target' ]   ]    
+        Y_train = master_train[ ['target' ]   ]
+           
 
 if __name__ == '__main__':
     risk = Risk()
-    risk.handle_data()
+    master_train = risk.handle_data_nan()
+    risk.handle_data_pre(master_train)
+    risk.train(master_train)
+    
+    
+    
+    
+    
+    
